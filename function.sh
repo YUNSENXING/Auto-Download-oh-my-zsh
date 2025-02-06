1b08df1864ab146fcf041a510c10d390b3a71d33
xysss <3891-xysss@users.noreply.git.acwing.com>
2025-02-06 17:42:18 +0800

    second version

---
+++ function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "download zsh..."
+sudo apt-get update
+sudo apt-get install -y zsh
+
+
+# use the install.sh 
+
+# if you cannot connect the github, you can modify the install.sh to connect the gitee version
+sed -i 's|REPO=${REPO:-ohmyzsh/ohmyzsh}|REPO=${REPO:-mirrors/oh-my-zsh}|' install.sh
+sed -i 's|REMOTE=${REMOTE:-https://github.com/${REPO}.git}|REMOTE=${REMOTE:-https://gitee.com/${REPO}.git}|' install.sh
+
+# add the execute function 
+chmod +x install.sh
+
+# modify the .zshrc file，change the ZSH_THEME = ys(change the theme)
+sed -i 's|ZSH_THEME=".*"|ZSH_THEME="ys"|' ~/.zshrc
+
+# source the .zshrc file to make the changes take effect
+source ~/.zshrc
+
+echo "操作完成！"
